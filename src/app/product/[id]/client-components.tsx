27bf0c2c49d673532f0f168141cd706ee07f10de
KallesenDK <kenneth@sigmatic.dk>
2025-04-10 19:32:08 +0200

    Beskrivelse af dine ændringer

--- src/app/product/[id]/client-components.tsx
+++ src/app/product/[id]/client-components.tsx
@@ -233,8 +233,8 @@ function ModernProductDetails({ product, hideProductId }: { product: Product, hi
     }
     
     // Ellers tjek om der er adresse eller postnummer på bruger
-    if (product.user?.postalCode) {
-      return `${product.user.postalCode.slice(0, 4)}`;
+    if (product.user?.postal_code) {
+      return `${product.user.postal_code.slice(0, 4)}`;
     }
     
     return 'Lokalitet ikke angivet';

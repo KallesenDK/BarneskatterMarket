082ae287a3abf1140058bc1d8b15a6144ef3b418
KallesenDK <kenneth@sigmatic.dk>
2025-05-03 18:52:34 +0200

    Change the way the user login

--- src/app/auth/login/route.ts
+++
@@ -1,3 +0,0 @@
-// The file has been intentionally left blank to avoid Next.js conflict, 
-// allowing only the login page (page.tsx) to handle /auth/login. 
-// This ensures that you can still log in via the UI.

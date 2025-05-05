f0e5e9fd88e8e744a9e0bf734191fa6b943fdb15
KallesenDK <kenneth@sigmatic.dk>
2025-05-05 21:23:21 +0200

    dashboard

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -2,13 +2,11 @@
 export const dynamic = "force-dynamic";
 import SupabaseProvider from '@/components/SupabaseProvider';
 
-import Navbar from '@/components/Navbar';
 import AdminNavigation from './dashboard/admin/components/AdminNavigation';
 
 export default function DashboardGroupLayout({ children }: { children: React.ReactNode }) {
   return (
     <SupabaseProvider>
-      <Navbar />
       <AdminNavigation />
       {children}
     </SupabaseProvider>

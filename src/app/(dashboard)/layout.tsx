f71316a6b2388cf6acc13567dc1b9b2191e27a43
KallesenDK <kenneth@sigmatic.dk>
2025-05-05 17:12:19 +0200

    Syntax

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,4 +1,5 @@
 "use client";
+export const dynamic = "force-dynamic";
 import SupabaseProvider from '@/components/SupabaseProvider';
 
 export default function DashboardGroupLayout({ children }: { children: React.ReactNode }) {
